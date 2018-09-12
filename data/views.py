d1921cb9faded73a0a99cdf0ce56753adfc04ff0
June Muoti <june.muoti@gmail.com>
2018-09-12 10:22:59 +0300

    first view function

---
+++ data/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import User
+
+# Create your views here.
+def index(request):
+    user=User.objects.all()
+    return render(request,'index.html',{"user":user})
